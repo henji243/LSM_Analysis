be8095eaa869be772c7a97b299495d9a48dfde42
henji <105550500+henji243@users.noreply.github.com>
2022-10-08 22:47:34 +0900

    変数名の変更
    変な変数名でした

--- LSM_Analysis.py
+++ LSM_Analysis.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from bs4 import element
+from pprint import pprint
 
 class LSM_Analysis:
     def __init__(self, path) -> None:
@@ -17,30 +18,30 @@ class LSM_Analysis:
         failed_aseets = [elem.text for elem in section.find_all("a", attrs={"data-lomtag":"failed"})]
         return failed_aseets
 
-    def warning_list(self):
+    def error_list(self):
         section = self._soup.select_one("body > section:nth-child(6)")
         tag = section.find_all("a", attrs={"data-lomtag":"warning"})
+
+        error_aseets = list()
         warning_aseets = list()
         for i in tag:
             if self.iserror(i):
-                warning_aseets.append(i)
-        return warning_aseets
+                error_aseets.append(i.text)
+            else:
+                warning_aseets.append(i.text)
+        return error_aseets
 
     def iserror(self, elem: element.Tag):
-        wtf = elem.find_parents("div")
-        parent = wtf[1].previous_siblings
-        string = ""
+        parent = elem.find_parents("div")
+        parent = parent[1].previous_siblings
 
-        #  h2タグのみを取得
-        for j in parent:
-            if type(j) is element.Tag:
-                string = string + str(j)
-        kenti = BeautifulSoup(string, "html.parser")
-        text = [j.text for j in kenti.find_all("h2")]
+        string = "".join(map(str, parent))
+        # なぜかうまくいかない為、一回またsoupを作る
+        soup = BeautifulSoup(string, "html.parser")
+        text = [j.text for j in soup.find_all("h2")]
         return len(text) == 2
 
 if __name__ == "__main__":
-    loading = LSM_Analysis(r"PATH")
-    aseet = loading.warning_list()
-    print(aseet)
-
+    loading = LSM_Analysis(input("PATH>>"))
+    aseet = loading.error_list()
+    pprint(aseet)
